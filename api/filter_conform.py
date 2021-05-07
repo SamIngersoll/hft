2f062a08f3ee95bde1de851c22e163e82232c8cc
Axel-Jacobsen <axelnj44@gmail.com>
2021-05-06 18:07:05 -0700

    wiating for binance verification

--- api/filter_conform.py
+++ api/filter_conform.py
@@ -13,3 +13,16 @@ def get_exchange_info_for_symbol(client: Client, symbol: str) -> Optional[Dict]:
         if symbol_data["symbol"] == symbol:
             return symbol_data
     return None
+
+def passes_filters(client: Client, symbol: str, price: float, quantity: float):
+    exchange_info = get_exchange_info_for_symbol(client, symbol)
+    
+    precision = exchange_info["baseAssetPrecision"]
+    
+    for flt in exchange_info["filters"]:
+        print(flt)
+
+def pass_price_filter(filter_conditions: Dict, price: float) -> bool:
+    within_price_range = filter_conditions["minPrice"] <= price <= filter_conditions["maxPrice"]
+    price_mod_ticksize = (price - minPrice) % filter_conditions["tickSize"] == 0
+    return within_price_range and price_mod_ticksize
