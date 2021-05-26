e6a8e2243097c287119514b412b78d5e139443ad
Axel-Jacobsen <axelnj44@gmail.com>
2021-05-25 20:04:13 -0700

    buncha stuff dunno whats the difference

--- ema_trader.py
+++ ema_trader.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+from enum import Enum
 from typing import List, Tuple, Optional
 
 
@@ -9,6 +10,10 @@ class EMA_trader:
     Why even have a class for this?
     - Easier management of previous EMA values, prices values, e.t.c.
     """
+    class OrderSide(Enum):
+        BUY = 1
+        SELL = 2
+        NO_OP = 3
 
     def __init__(self, ema_smoothing_const: float = 0.1, period_range: int = 2):
         """
@@ -22,18 +27,6 @@ class EMA_trader:
         self.period_range_2: int = period_range - 1
         self._prev_ema_val_1: Optional[float] = None  # default initial EMA value to 0
         self._prev_ema_val_2: Optional[float] = None  # default initial EMA value to 0
-
-    def set_params(
-        self,
-        ema_smoothing_const_1: float,
-        period_range_1: float,
-        ema_smoothing_const_2: int,
-        period_range_2: int,
-    ):
-        self.ema_smoothing_const_1: float = ema_smoothing_const_1
-        self.ema_smoothing_const_2: float = ema_smoothing_const_2
-        self.period_range_1: float = period_range_1
-        self.period_range_2: float = period_range_2
 
     def ema(
         self,
@@ -50,37 +43,56 @@ class EMA_trader:
         "Simply return EMA values at given cur_price"
 
         # if this is the first call to EMA, set the previous ema to cur price
-        if self._prev_ema_val_1 is None or self._prev_ema_val_2 is None:
-            self._prev_ema_val_1 = cur_price
-            self._prev_ema_val_2 = cur_price
+        self._prev_ema_val_1 = (
+            cur_price if self._prev_ema_val_1 is None else self._prev_ema_val_1
+        )
+        self._prev_ema_val_2 = (
+            cur_price if self._prev_ema_val_2 is None else self._prev_ema_val_2
+        )
 
-        ema_1 = self.ema(
+        ema1 = self.ema(
             cur_price,
             self._prev_ema_val_1,
             self.ema_smoothing_const_1,
             self.period_range_1,
         )
-        ema_2 = self.ema(
+        ema2 = self.ema(
             cur_price,
             self._prev_ema_val_2,
             self.ema_smoothing_const_2,
             self.period_range_2,
         )
-        return ema_1, ema_2
 
-    def action(self, cur_price: float) -> float:
+        self._prev_ema_val_1 = ema1
+        self._prev_ema_val_2 = ema2
+
+        return ema1, ema2
+
+    def action(self, cur_price: float) -> int:
         """
         Returns:
-            1 if should buy
-            0 if do nothing
+             1 if should buy
+             0 if do nothing
             -1 if should sell
         """
         ema1, ema2 = self.ema_pair(cur_price)
-        if ema1 > ema2:
-            return 1
-        elif ema2 > ema1:
-            return -1
-        return 0
+        if ema2 > ema1:
+            return self.OrderSide.BUY
+        elif ema1 > ema2:
+            return self.OrderSide.SELL
+        return self.OrderSide.NO_OP
+
+    def set_params(
+        self,
+        ema_smoothing_const_1: float,
+        period_range_1: float,
+        ema_smoothing_const_2: int,
+        period_range_2: int,
+    ):
+        self.ema_smoothing_const_1: float = ema_smoothing_const_1
+        self.ema_smoothing_const_2: float = ema_smoothing_const_2
+        self.period_range_1: float = period_range_1
+        self.period_range_2: float = period_range_2
 
     def optimize(self, prices: np.array):
         """
@@ -93,61 +105,58 @@ class EMA_trader:
         best_ema_smoothing_const_2 = 0
         best_period_range_2 = 0
 
-        for j in range(0, 10):
-            for k in range(0, 10):
-                for l in range(0, 10):
-                    for m in range(0, 10):
-                        cash = 100
-                        doge = 0
+        prev_price, *prices = prices
+
+        for j in range(10):
+            for k in range(10):
+                for l in range(10):
+                    for m in range(10):
+                        base = 100
+                        quote = 0
 
                         d1 = j
                         d2 = k
-
                         ema_smoothing_const_1 = l / 10
                         ema_smoothing_const_2 = m / 10
 
-                        ema_hist1 = [0] * len(t)
-                        ema_hist2 = [0] * len(t)
-                        ema_hist1[0] = p[0]
-                        ema_hist2[0] = p[0]
+                        prev_ema1 = prev_price
+                        prev_ema2 = prev_price
 
-                        for i in range(1, len(p)):
-                            price = p[i]
+                        for i, price in enumerate(prices):
 
                             # trading logic
-                            ema_hist1[i] = self.ema(
-                                price, ema_hist1[i - 1], ema_smoothing_const_1, d1
-                            )
-                            ema_hist2[i] = self.ema(
-                                price, ema_hist2[i - 1], ema_smoothing_const_2, d2
-                            )
+                            ema1 = self.ema(price, prev_ema1, ema_smoothing_const_1, d1)
+                            ema2 = self.ema(price, prev_ema2, ema_smoothing_const_2, d2)
 
                             percent_change = (price - prev_price) / prev_price
-                            doge *= 1 + percent_change
+                            quote *= 1 + percent_change
 
-                            if ema_hist2[i] < ema_hist1[i]:
+                            if ema1 > ema2:
                                 # buy
-                                if cash > 0:
-                                    doge += 0.999 * cash
-                                    cash -= cash
-                                    activity[i] = buy
-
-                            if ema_hist2[i] > ema_hist1[i]:
+                                if base > 0:
+                                    quote += 0.999 * base
+                                    base = 0
+                            elif ema2 > ema1:
                                 # sell
-                                if doge > 0:
-                                    cash += 0.999 * doge
-                                    doge -= doge
-                                    activity[i] = sell
+                                if quote > 0:
+                                    base += 0.999 * quote
+                                    quote = 0
 
                             prev_price = price
 
-                        if doge + cash > best_score:
-                            best_score = doge + cash
+                        if quote + base > best_score:
+                            best_score = quote + base
                             best_period_range_1 = d1
                             best_period_range_2 = d2
                             best_ema_smoothing_const_1 = ema_smoothing_const_1
                             best_ema_smoothing_const_2 = ema_smoothing_const_2
 
+        print(f"{best_score:.3f}", (
+            best_ema_smoothing_const_1,
+            best_period_range_1,
+            best_ema_smoothing_const_2,
+            best_period_range_2,
+        ))
         return (
             best_ema_smoothing_const_1,
             best_period_range_1,
