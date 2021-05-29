bd3a299f7e8703273960e15c26ebda4df11ecd92
Axel-Jacobsen <axelnj44@gmail.com>
2021-05-28 18:38:19 -0700

    beginner's luck

--- bl.py
+++ bl.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python3
 
+import argparse
 import time, toml
 import numpy as np
 
@@ -7,42 +8,60 @@ from trader import Trader
 
 from binance import ThreadedWebsocketManager
 
-from twisted.internet import reactor
+
+parser = argparse.ArgumentParser("beginner's luck")
+parser.add_argument(
+    "symbol",
+    metavar="symbol",
+    type=str,
+    help="ticker symbol - e.g. DOGEBTC. Note: does not error for invalid symbols",
+)
+parser.add_argument(
+    "-p", metavar="--print_period", type=float, help="period between printing info to stdout [s]", default=60
+)
+parser.add_argument(
+    "-o", metavar="--optimzation_period", type=int, help="number of prices over which we optimize [trades]", default=1000
+)
 
 
 if __name__ == "__main__":  # everything in main block due to multiprocessing errors
+    args = parser.parse_args()
+    symbol = args.symbol
+
+    t = Trader(async_optimization=True, optimization_period=args.o)
+
     cfg = toml.load("api/configuration.toml")
     pkey = cfg["auth"]["pkey"]
     skey = cfg["auth"]["skey"]
 
-    t = Trader(async_optimization=True, optimization_period=5000)
-
-    def cb(bm, stream_name, msg):
+    def cb(bm, symbol, msg):
         if msg["e"] == "error":
             # close and restart the socket
             print(f"websocket error encountered: {msg['e']}")
             print(f"attempting restart...")
-            bm.stop_socket(stream_name)
+            bm.stop_socket(symbol)
             bm.start_symbol_ticker_socket(
-                symbol="DOGEBTC", callback=lambda msg: cb(bm, "DOGEBTC", msg)
+                symbol=symbol, callback=lambda msg: cb(bm, symbol, msg)
             )
-        t.trade(float(msg["c"]))
+        else:
+            t.trade(float(msg["c"]))
+
+    print(f"starting socket for {symbol}...")
+    bm = ThreadedWebsocketManager(api_key=pkey, api_secret=skey)
+    bm.start()
+    bm.start_symbol_ticker_socket(
+        symbol=symbol, callback=lambda msg: cb(bm, symbol, msg)
+    )
+
+    print("socket started\n")
 
     try:
-        print("starting socket...")
-        bm = ThreadedWebsocketManager(api_key=pkey, api_secret=skey)
-        bm.start()
-        bm.start_symbol_ticker_socket(
-            symbol="DOGEBTC", callback=lambda msg: cb(bm, "DOGEBTC", msg)
-        )
-        print("socket started\n")
-
         while True:
             print("\n" + time.ctime())
             print(f"Price History Len: {len(t.price_history)}")
             print(f"base: {t.base}\t\tquote: {t.quote}")
             print(f"buys: {t.num_buys}\t\tsells: {t.num_sells}\n")
-            time.sleep(60)
+            time.sleep(args.p)
 
     except KeyboardInterrupt:
         # halt the worker pool for optimization
